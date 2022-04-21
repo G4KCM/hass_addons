ac517ad1abf09df8e0beba89dfa618b02a10118b
posixx <2280400+posixx@users.noreply.github.com>
2022-04-21 13:23:15 +0200

    data location change

--- deepharmony/run.sh
+++ deepharmony/run.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-until env DEBUG="*" ./data/roon-extension-deep-harmony; do
+cd /data
+until env DEBUG="*" ./roon-extension-deep-harmony; do
     echo "roon-extension-deep-harmony terminated with exit code $?.  Restarting.." >&2
-    if [ -f ./data/roon-extension-deep-harmony.old ]; then
-        rm ./data/roon-extension-deep-harmony.old
+    if [ -f ./roon-extension-deep-harmony.old ]; then
+        rm ./roon-extension-deep-harmony.old
     fi
-    if [ -f ./data/downloads/roon-extension-deep-harmony ]; then
-        mv ./data/roon-extension-deep-harmony ./data/roon-extension-deep-harmony.old
-        mv ./data/downloads/roon-extension-deep-harmony ./data/roon-extension-deep-harmony
-        chmod 777 ./data/roon-extension-deep-harmony
-        rm ./data/readme.html
-        mv ./data/downloads/readme.html ./readme.html
+    if [ -f ./downloads/roon-extension-deep-harmony ]; then
+        mv ./roon-extension-deep-harmony ./roon-extension-deep-harmony.old
+        mv ./downloads/roon-extension-deep-harmony ./roon-extension-deep-harmony
+        chmod 777 ./roon-extension-deep-harmony
+        rm ./readme.html
+        mv ./downloads/readme.html ./readme.html
     fi
     sleep 1
 done
