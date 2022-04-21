2d9fc600647d08c95074892cfbe370581e4df3e1
posixx <2280400+posixx@users.noreply.github.com>
2022-04-21 14:48:15 +0200

    location changes

--- deepharmony/run.sh
+++ deepharmony/run.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-cd /addons
-ls -las
+if [  ! -d /share/roon-extension-deep-harmony ]; then
+    mkdir /share/roon-extension-deep-harmony
+    cp roon-extension-deep-harmony /share/roon-extension-deep-harmony/
+    chmod 777 /share/roon-extension-deep-harmony/roon-extension-deep-harmony
+    cp readme.html /share/roon-extension-deep-harmony/
+fi
+cd /share/roon-extension-deep-harmony/
 until env DEBUG="*" ./roon-extension-deep-harmony; do
     echo "roon-extension-deep-harmony terminated with exit code $?.  Restarting.." >&2
     if [ -f ./roon-extension-deep-harmony.old ]; then
