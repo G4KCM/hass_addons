588f0dbba0d61b10cf13188d05894109d4e43ef6
posixx <2280400+posixx@users.noreply.github.com>
2022-12-24 15:03:41 +0100

    Cleanup actions

--- envoy2mqtt/run.sh
+++ envoy2mqtt/run.sh
@@ -5,7 +5,6 @@ MQTTBROKERHOST=$(bashio::services mqtt "host")
 MQTTBROKERPORT=$(bashio::services mqtt "port")
 MQTTUSERNAME=$(bashio::services mqtt "username")
 MQTTPASSWORD=$(bashio::services mqtt "password")
-ALPHA2IPS=$(jq --raw-output ".alpha2ips" $CONFIG_PATH)
-REFRESHSECONDS=$(jq --raw-output ".refreshseconds" $CONFIG_PATH)
-
-python3 -u ./alpha2.py $MQTTBROKERHOST $MQTTBROKERPORT $MQTTUSERNAME $MQTTPASSWORD $ALPHA2IPS $REFRESHSECONDS
+ENVOYIP=$(jq --raw-output ".envoyip" $CONFIG_PATH)
+ENVOYPASS=$(jq --raw-output ".envoypass" $CONFIG_PATH)
+python3 -u ./envoy.py $MQTTBROKERHOST $MQTTBROKERPORT $MQTTUSERNAME $MQTTPASSWORD $ENVOYIP $ENVOYPASS
